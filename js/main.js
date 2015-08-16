ccbc47e913f821b60b7edb361c49d99a0f723046
Jes Gundy <jesse.gundy@gmail.com>
2015-08-15 21:04:51 -0700

    added inline templates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
+// Document Ready
 $(function() {
 
-  console.log($);
-  console.log(_);
-  console.log(Backbone);
+
+
+  // Flickr Search Constructor
+  var FlickrSearch = Backbone.View.extend({
+    el: '.container',
+
+
+    initialize: function () {
+      this.getInlineTemplates();
+      this.render();
+    },
+
+
+    getInlineTemplates: function () {
+      this.placeholderTemplate = _.template( $('#placeholderTemplate').html() );
+      this.noResultsTemplate = _.template( $('#noResultsTemplate').html() );
+      this.photosTemplate = _.template( $('#photosTemplate').html() );
+    },
+
+
+    render: function () {
+      this.$('.results').html( this.placeholderTemplate() );
+    }
+  });
+
+
+
+  // Instantiate
+  var flickrSearch = new FlickrSearch();
+
+
 
 });
